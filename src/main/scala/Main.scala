cf26cb87817b89cc75ded5ba58b91c32f3b49598
NNLisko <niko.1lausto2@gmail.com>
2026-04-14 20:27:16 +0300

    first printmenu

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,28 @@
+// SCALA PROJECT | RENEWABLE POWERPLANT
+
+// Contributors:
+// Niko Lausto            || 001890439
+// Emilija Kurtinaityte   || 
+// Adelina Stan           || 
+
+// AI statement: 
+
+
+// If build tools won't work, compiles with:
+// scalac src/main/scala/Main.scala src/main/scala/menu/Menus.scala 
+// and runs with:
+// scala src/main/scala/Main.scala
+
+
+import menus.Menus
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    
+
+    // the main probably should only call to display menu
+    Menus.showMainMenu()
+
+  }
+}
+
---
+++ src/main/scala/menu/Menus.scala
@@ -0,0 +1,62 @@
+package menus
+
+// menu inspo from https://stackoverflow.com/questions/44461625/commandline-menu-loop-scala
+
+import scala.annotation.tailrec
+
+
+object Menus {
+
+    @tailrec
+    def showMainMenu(): Unit = {
+
+        println("""
+        .------ Welcome to a renewabkle energy plant system ------.
+        |                                                         |
+        > 1. Display energy output and statistics                 |
+        > 2. Display the control menu                             |
+        > 3. Exit                                                 |
+        |                                                         |
+        '---------------------------------------------------------'
+        """.stripMargin)
+
+        scala.io.StdIn.readLine("Select: ").trim match {
+            case "1" => /* CALL METRICS SUB MENU */ ; showMainMenu()
+            case "2" => /* CALL CONTROL PANEL SUB MENUY */ ; showMainMenu()
+            case "3" => println("Shutting down")
+            case _ => showMainMenu()
+        }
+    }
+
+    // i figured if we decide to have multiple nested submenus, they can be called just ControlPanelMenu2 etc
+
+    @tailrec
+    def showControlPanelMenu1(): Unit = {
+
+        println("""
+            menu here
+        """)
+
+        scala.io.StdIn.readLine("Select: ").trim match {
+            case "1" => 
+            case "2" => 
+            case "3" => 
+            case _ => showControlPanelMenu1()
+        }
+    }
+
+    @tailrec
+    def showEnergyMetricsMenu1(): Unit = {
+
+        println("""
+            menu here
+        """)
+
+        scala.io.StdIn.readLine("Select: ").trim match {
+            case "1" => 
+            case "2" => 
+            case "3" => 
+            case _ => showEnergyMetricsMenu1()
+        }
+    }
+}
